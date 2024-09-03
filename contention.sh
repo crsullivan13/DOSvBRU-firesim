0cd9a9863abc931cdb2c3ec9c286d1d9e5432c54
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-09-03 11:50:31 -0500

    Add delay between workloads, resulsts are inconsistent otherwise

--- contention.sh
+++ contention.sh
@@ -9,6 +9,7 @@ sed -i "48 c\\    default_hw_config: vcu118_max_mempress_4GB_2Bank_bru" deploy/c
 
 for i in "${workloads[@]}"; do
 	echo "$i"
-	sed -i "85 c\\    workload_name: $i" deploy/config_runtime.yaml	
+	sed -i "85 c\\    workload_name: $i" deploy/config_runtime.yaml
+	sleep 10	
 	firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
 done
