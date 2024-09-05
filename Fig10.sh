049e0797bcaa731a31687caaf437a0ade7672b77
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-09-05 13:18:32 -0500

    update workload name

--- Fig10.sh
+++ Fig10.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-workloads=("1280.json")
-
 OUT=Graphing-BankDefense/Fig10-dirs.txt
 > $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_2Bank_bru" deploy/config_runtime.yaml
 
 echo "2bk-all"
-sed -i "85 c\\    workload_name: bandwidth-$i" deploy/config_runtime.yaml	
+sed -i "85 c\\    workload_name: bandwidth-1280.json" deploy/config_runtime.yaml	
 firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUTiresim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_2Bank_bru-aware" deploy/config_runtime.yaml
