2de71b4adce722c1c9281979f1a2c1647c4b0343
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-09-05 12:37:39 -0500

    Fix errors and consolidate

--- Fig7-9.sh
+++ Fig7-9.sh
@@ -2,10 +2,21 @@
 
 workloads=("contention-solo.json" "contention-sepBankW.json" "contention-sameBankW.json" "contention-throttledW.json")
 
-OUT=Graphing-BankDefense/Fig7-dirs.txt
+OUT=Graphing-BankDefense/Fig7-9-dirs.txt
 > $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_max_mempress_4GB_2Bank_bru" deploy/config_runtime.yaml	
+
+for i in "${workloads[@]}"; do
+	echo "$i"
+	sed -i "85 c\\    workload_name: $i" deploy/config_runtime.yaml
+	sleep 30	
+	firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
+done
+
+sleep 30
+
+sed -i "48 c\\    default_hw_config: vcu118_max_mempress_4GB_2Bank_bru-aware" deploy/config_runtime.yaml	
 
 for i in "${workloads[@]}"; do
 	echo "$i"
