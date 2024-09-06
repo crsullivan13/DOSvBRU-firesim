f6e863367ab0814ac75d8ba1c7b201a33de8fc32
Connor Sullivan <connor.r.sullivan9922@gmail.com>
2024-09-06 16:12:10 -0500

    Fix fig10 script errors

--- Fig10.sh
+++ Fig10.sh
@@ -7,22 +7,22 @@ sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_2Bank_bru" deploy/config_
 
 echo "2bk-all"
 sed -i "85 c\\    workload_name: bandwidth-1280.json" deploy/config_runtime.yaml	
-firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUTiresim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
+firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_2Bank_bru-aware" deploy/config_runtime.yaml
 
 sleep 30
 echo "2bk-per"
-firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUTiresim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
+firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_4Bank_bru" deploy/config_runtime.yaml
 
 sleep 30
 echo "4bk-all"
-firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUTiresim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
+firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
 
 sed -i "48 c\\    default_hw_config: vcu118_lgboom_4GB_4Bank_bru-aware" deploy/config_runtime.yaml
 
 sleep 30
 echo "4bk-per"
-firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUTiresim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
+firesim infrasetup && firesim runworkload | grep results-workload | tail -n 1 >> $OUT
